2a7f07f8d3eef6d0d2ac1e27ffe9feae874f5dea
juliakasak12 <julia.kasak@pollub.edu.pl>
2025-05-27 19:19:29 +0200

    Add files via upload

---
+++ tests/login_test.py
@@ -0,0 +1,70 @@
+from tests.base_test import BaseTest
+from faker import Faker
+
+
+class LoginTest(BaseTest):
+
+    def setUp(self):
+        super().setUp()
+        # Dodatkowy warunek wstępny - wejście na stronę logowania
+        self.login_page = self.home_page.click_log_in()
+
+
+
+    def testEmptyLogin(self):
+        # (nie wpisujemy nic)
+        # 1. Kliknij LogIn
+        self.login_page.click_log_in1()
+        # 2. Zamknij alert
+        self.login_page.close_alert()
+
+    def testInvalidLogin(self):
+        #(Niepoprawny login)
+        username = "tester_alk"
+        password = "haslo"
+        # 1. Wpisz login
+        self.login_page.enter_username(username)
+        # 2. Wpisz haslo
+        self.login_page.enter_password(password)
+        # 3. Kliknij Log In
+        self.login_page.click_log_in1()
+        # 4. Zamknij alert
+        self.login_page.close_alert()
+
+    def testInvalidPassword(self):
+        #(Niepoprawne hasło)
+        username = "testeralk8@gmail.com"
+        password = "ala123"
+        # 1. Wpisz login
+        self.login_page.enter_username(username)
+        # 2. Wpisz haslo
+        self.login_page.enter_password(password)
+        # 3. Kliknij Log In
+        self.login_page.click_log_in1()
+        # 4. Zamknij alert
+        self.login_page.close_alert()
+
+    def testRandomData(self):
+        #(Dane z biblioteki Faker)
+        fake = Faker(locale='pl')
+        username = fake.email()
+        password = fake.password()
+        # 1. Wpisz login
+        self.login_page.enter_username(username)
+        # 2. Wpisz haslo
+        self.login_page.enter_password(password)
+        # 3. Kliknij Log In
+        self.login_page.click_log_in1()
+        # 4. Zamknij alert
+        self.login_page.close_alert()
+
+    def testValidData(self):
+        #(Poprawne dane logowania)
+        username = "testeralk8@gmail.com"
+        password = "Testeralk@88"
+        # 1. Wpisz login
+        self.login_page.enter_username(username)
+        # 2. Wpisz haslo
+        self.login_page.enter_password(password)
+        # 3. Kliknij Log In
+        self.login_page.click_log_in1()
