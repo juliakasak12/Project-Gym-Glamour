e6e68e8e2b8bb70a0a22ce98c1048c06ae89f77f
juliakasak12 <julia.kasak@pollub.edu.pl>
2025-06-14 23:20:50 +0200

    Delete tests directory

--- tests/home_test.py
+++
@@ -1,8 +0,0 @@
-from tests.base_test import BaseTest
-
-class HomeTest(BaseTest):
-    def test_tittle(self):
-        driver = self.driver
-        page_title = driver.title
-        #1.Porównaj tytuł strony internetowej
-        self.assertIn("Gym Glamour", page_title, "Page tittle does not contain 'GymGlamour'")
